1f6116a4b1e05f4658105e3dc7adc5c67d192171
hojjat zangeneh <hojjat.zangeneh@LIVE.COM>
2023-04-28 21:49:52 +0330

    Update Home Screen

--- turboplus/lib/screens/home_screen.dart
+++ turboplus/lib/screens/home_screen.dart
@@ -15,43 +15,93 @@ class _HomeScreenState extends State<HomeScreen> {
       body: SafeArea(
         child: Column(
           children: [
-            Container(
-              decoration: const BoxDecoration(
-                color: Color(0xff121212),
+            headerHome(),
+            const SizedBox(height: 10),
+            SizedBox(
+              height: 96,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) => StoryWidget(),
               ),
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child:
-                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  const Icon(
-                    Icons.camera_alt_outlined,
-                    color: Colors.white,
-                    size: 30,
-                  ),
-                  const Spacer(),
-                  Image.asset('assets/images/InstagramLogoSmall.png'),
-                  const Spacer(),
-                  const Icon(
-                    Icons.tv_outlined,
-                    color: Colors.white,
-                    size: 30,
-                  ),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  const Icon(
-                    Icons.messenger_outlined,
-                    color: Colors.white,
-                    size: 30,
-                  ),
-                ]),
-              ),
-            )
+            ),
           ],
         ),
+      ),
+    );
+  }
+
+  Container headerHome() {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Color(0xff121212),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          const SizedBox(
+            width: 10,
+          ),
+          const Icon(
+            Icons.camera_alt_outlined,
+            color: Colors.white,
+            size: 30,
+          ),
+          const Spacer(),
+          Image.asset('assets/images/InstagramLogoSmall.png'),
+          const Spacer(),
+          const Icon(
+            Icons.tv_outlined,
+            color: Colors.white,
+            size: 30,
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          const Icon(
+            Icons.messenger_outlined,
+            color: Colors.white,
+            size: 30,
+          ),
+        ]),
+      ),
+    );
+  }
+}
+
+class StoryWidget extends StatelessWidget {
+  const StoryWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10),
+      child: Column(
+        children: [
+          Container(
+              width: 62,
+              height: 62,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  gradient: const LinearGradient(
+                    colors: [
+                      Color(0xffFBAA47),
+                      Color(0xffD91A46),
+                      Color(0xffA60F93),
+                    ],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                  )),
+              child: Image.asset('assets/images/User1.png')),
+          const SizedBox(
+            height: 5,
+          ),
+          const Text(
+            "Your Story",
+            style: TextStyle(color: Colors.white),
+          ),
+        ],
       ),
     );
   }
